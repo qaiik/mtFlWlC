b12b9e6bb2ffec35b200cf1b640e9a6309201214
qaiik <82181231+qaiik@users.noreply.github.com>
2023-04-12 15:09:35 -0600

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,14 +1,18 @@
 function calculate(start, end, cores) {
-  const start = start;
-  const end = end;
   const range = end - start;
   const step = ~~(range / cores)
-  const output = []
+  let output = []
   for (let i = start; i < end; i += step) {
     output.push({
       start: i,
       end: Math.min(i + step, end)
     })
+  }
+  const extra = output.slice(cores).map(wl => wl.end).reduce((a,b) => a + b, 0)
+  console.log(extra)
+  output = output.slice(0, cores)
+  if (extra / end >= 1) {
+    output[cores - 1].end = end;
   }
   return output
 }
